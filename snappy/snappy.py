31ab6d335ad9b082dde8c5ca05c10b6ccf38ae02
blazejsiejek <blazej.siejek@nordcloud.com>
2018-07-27 15:42:15 +0200

    Handle exceptions from AWS when instance is in pending state

--- snappy/snappy.py
+++ snappy/snappy.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 # specify profile from ~/.aws/credentials to be used
@@ -146,7 +147,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}..".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -159,7 +164,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}..".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}. ".format(i.id) + str(e))
+        continue
 
     return
 
