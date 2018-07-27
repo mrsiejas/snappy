713b0c54bc56265e2c6eb5c002b0b290560ed2b0
blazejsiejek <blazej.siejek@nordcloud.com>
2018-07-27 15:57:28 +0200

    List all snaps option added

--- snappy/snappy.py
+++ snappy/snappy.py
@@ -29,8 +29,10 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
     help="Only snapshots for project (tag Project:<name>)")
+@click.option('--all', 'list_all', default=False, is_flag=True,
+    help="List all snapshots for each volume, not just the most recent")
 
-def list_snapshots(project): # list volumes here?
+def list_snapshots(project, list_all):
     "Lists EC2 snapshots"
 
     instances = filter_instances(project)
@@ -46,6 +48,8 @@ def list_snapshots(project): # list volumes here?
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+
+                if s.state == 'completed' and not list_all: break
 
     return
 
