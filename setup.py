ae07f70fb5fa8fa094f54726d81287edc18e5fed
Blazej Siejek <40015521+mrsiejas@users.noreply.github.com>
2020-10-02 09:04:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 setup(
     name='snappy',
     version='0.1',
-    author="Blazej Siejek",
-    author_email="blazej.siejek@nordcloud.com",
+    author="mrsiejas",
+    author_email="dummy@email.com",
     description="Snappy is a tool to manage AWS EC2 snapshots",
     license="GPLv3+",
     packages=['snappy'],
-    url='https://github.com/blazejss/snappy',
+    url='https://github.com/mrsiejas/snappy',
     install_requires=[
         'click',
         'boto3'
